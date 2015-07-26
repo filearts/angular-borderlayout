00bbe68e45fac81c39c25ddacb254b8c75811074
e-cloud <saintscott119@gmail.com>
2015-07-26 21:32:31 +0800

    * improve faPaneResizer directive for more organization * fix a bug for clicking twice to toggle the resizer

--- src/borderLayout.js
+++ src/borderLayout.js
@@ -705,138 +705,135 @@
 		}
 	]);
 
-	module.directive("faPaneResizer", [
-		"$window", function ($window) {
+	module.directive("faPaneResizer", ["$window", function ($window) {
 
-			return {
-				restrict: "A",
-				//require: "^faPane",
-				link: function ($scope, $element, $attrs, $pane) {
-					// return unless $pane
-					$pane || ($pane = $scope.$pane);
+		return {
+			restrict: "A",
+			//require: "^faPane",
+			link: function ($scope, $element, $attrs, $pane) {
+				// return unless $pane
+				$pane || ($pane = $scope.$pane);
 
-					var el = $element[0];
+				var el = $element[0];
 
-					var clickRadius = 5;
-					var clickTime = 300;
+				var clickRadius = 5;
+				var clickTime = 300;
 
-					$scope.$watch((function () {
-						return $pane.getOrientation();
-					}), function (orientation) {
-						$element.removeClass("vertical");
-						$element.removeClass("horizontal");
+				$scope.$watch((function () {
+					return $pane.getOrientation();
+				}), function (newOrientation) {
+					switch (newOrientation) {
+						case "vertical":
+							$element.removeClass("horizontal");
+							$element.addClass("vertical");
+							break;
+						case "horizontal":
+							$element.addClass("horizontal");
+							$element.removeClass("vertical");
+							break;
+					}
+				});
 
-						switch (orientation) {
-							case "vertical":
-								return $element.addClass("vertical");
-							case "horizontal":
-								return $element.addClass("horizontal");
-						}
-					});
+				el.addEventListener("mousedown", function (e) {
+					if (e.button !== 0) {
+						return;
+					}
 
-					return el.addEventListener("mousedown", function (e) {
-						if (e.button !== 0) {
-							return;
-						}
+					var anchor = $pane.anchor;
+					var coord;
+					if (anchor === "north" || anchor === "south") {
+						coord = "screenY";
+					} else if (anchor === "west" || anchor === "east") {
+						coord = "screenX";
+					}
 
-						var anchor = $pane.anchor;
-						var coord;
-						if (anchor === "north" || anchor === "south") {
-							coord = "screenY";
-						} else if (anchor === "west" || anchor === "east") {
-							coord = "screenX";
-						}
+					var scale;
+					if (anchor === "north" || anchor === "west") {
+						scale = 1;
+					} else if (anchor === "south" || anchor === "east") {
+						scale = -1;
+					}
 
-						var scale;
-						if (anchor === "north" || anchor === "west") {
-							scale = 1;
-						} else if (anchor === "south" || anchor === "east") {
-							scale = -1;
-						}
+					var startPos = {
+						x: e.screenX,
+						y: e.screenY
+					};
+					var startCoord = e[coord];
+					var startSize = $pane.size;
+					var startTime = Date.now();
 
-						var startPos = {
-							x: e.screenX,
-							y: e.screenY
-						};
-						var startCoord = e[coord];
-						var startSize = $pane.size;
-						var startTime = Date.now();
+					//pane.onHandleDown();
 
-						//pane.onHandleDown();
+					// Not sure if this really adds value, but added for compatibility
+					el.unselectable = "on";
+					el.onselectstart = function () {
+						return false;
+					};
+					el.style.userSelect = el.style.MozUserSelect = el.style.msUserSelect = el.style.webkitUserSelect = "none";
 
-						// Not sure if this really adds value, but added for compatibility
-						el.unselectable = "on";
-						el.onselectstart = function () {
-							return false;
-						};
-						el.style.userSelect = el.style.MozUserSelect = "none";
+					// Null out the event to re-use e and prevent memory leaks
+					//e.setCapture();
+					e.preventDefault();
+					e.defaultPrevented = true;
+					e = null;
 
-						// Null out the event to re-use e and prevent memory leaks
+					var handleMouseMove = function (e) {
+						faDragged = true; // Fix for dragging on toggle
+
+						$pane.$onStartResize();
+
+						// Inside Angular's digest, determine the ideal size of the element
+						// according to movements then determine if those movements have been
+						// constrained by boundaries, other panes or min/max clauses
+						$scope.$apply(function () {
+							var targetSize = startSize + scale * (e[coord] - startCoord);
+
+							return $pane.resize(targetSize);
+						});
+
+						// Null out the event in case of memory leaks
 						//e.setCapture();
 						e.preventDefault();
 						e.defaultPrevented = true;
 						e = null;
+					};
 
-						var handleMouseMove = function (e) {
-							faDragged = true; // Fix for dragging on toggle
+					// Prevent the reflow logic from happening too often
+					var handleMouseMoveThrottled = _.throttle(handleMouseMove, 16);
 
-							$pane.$onStartResize();
+					var handleMouseUp = function (e) {
+						var displacementSq = Math.pow(e.screenX - startPos.x, 2) +
+							Math.pow(e.screenY - startPos.y, 2);
+						var timeElapsed = Date.now() - startTime;
 
-							// Inside Angular's digest, determine the ideal size of the element
-							// according to movements then determine if those movements have been
-							// constrained by boundaries, other panes or min/max clauses
-							$scope.$apply(function () {
-								var targetSize = startSize + scale * (e[coord] - startCoord);
+						$window.removeEventListener("mousemove", handleMouseMoveThrottled, true);
+						$window.removeEventListener("mouseup", handleMouseUp, true);
 
-								return $pane.resize(targetSize);
-							});
+						var cleanup = function () {
+							$pane.$onStopResize();
 
 							// Null out the event in case of memory leaks
-							//e.setCapture();
+							//e.releaseCapture();
 							e.preventDefault();
 							e.defaultPrevented = true;
-							return e = null;
+							e = null;
 						};
 
-						var handleMouseUp;
-						handleMouseUp = function (e) {
-							var displacementSq = Math.pow(e.screenX - startPos.x, 2) +
-								Math.pow(e.screenY - startPos.y, 2);
-							var timeElapsed = Date.now() - startTime;
-
-							$window.removeEventListener("mousemove", handleMouseMoveThrottled, true);
-							$window.removeEventListener("mouseup", handleMouseUp, true);
-
-							var cleanup = function () {
-								$pane.$onStopResize();
-
-								// Null out the event in case of memory leaks
-								//e.releaseCapture();
-								e.preventDefault();
-								e.defaultPrevented = true;
-								return e = null;
-							};
-
-							if (displacementSq <= Math.pow(clickRadius, 2) && timeElapsed <= clickTime) {
-								cleanup();
-								return;
-							}
-
+						if (!(displacementSq <= Math.pow(clickRadius, 2) && timeElapsed <= clickTime)) {
 							// In case the mouse is released at the end of a throttle period
 							handleMouseMove(e);
+						}
 
-							return cleanup();
-						};
+						cleanup();
+						faDragged = false; // Fix for dragging on toggle
+					};
 
-						// Prevent the reflow logic from happening too often
-						var handleMouseMoveThrottled = throttle(10, handleMouseMove);
-
-						$window.addEventListener("mouseup", handleMouseUp, true);
-						return $window.addEventListener("mousemove", handleMouseMoveThrottled, true);
-					});
-				}
-			};
-		}
+					$window.addEventListener("mouseup", handleMouseUp, true);
+					$window.addEventListener("mousemove", handleMouseMoveThrottled, true);
+				});
+			}
+		};
+	}
 	]);
 
 	return module
