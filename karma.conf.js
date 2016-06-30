591ca0006de634fbfd841fa88da1484af7b8adbe
e-cloud <saintscott119@gmail.com>
2016-06-30 19:10:57 +0800

    feat: update karma conf to support test notifier

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function (config) {
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'mocha', 'coverage'],
+		reporters: ['notify', 'mocha', 'coverage'],
 
 		coverageReporter: {
 			type: 'html',
@@ -64,6 +64,7 @@ module.exports = function (config) {
 			'karma-sinon',
 			'karma-mocha',
 			'karma-mocha-reporter',
+			'karma-notify-reporter',
 			'karma-coverage' // required for coverage
 		],
 
